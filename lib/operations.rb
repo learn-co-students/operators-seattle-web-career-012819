971905e3a3efcd6ec0c55f3acacaaff326e403b7
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-10 03:13:39 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,19 @@
 def unsafe?(speed)
-
+  if (speed < 40 || speed > 60)
+    return true
+  else
+    return false
+  end
 end
 
 
 
 def not_safe?(speed)
+  (speed < 40) || (speed > 60) ?
+  answer = true:
+  answer = false
+  
+  return answer
 	
 end
 	
